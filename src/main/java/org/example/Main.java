2200a4d4740bfd764877baca6081ba903a6b837a
radam <radamir33@mail.ru>
2022-06-21 23:35:42 +0300

    hw1_java_core_2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-
+import java.util.Random;
 
 public abstract class Main {
 
@@ -14,15 +14,25 @@ public abstract class Main {
         cat.run();
         robot.jump();
         robot.run();*/
+        Person person = new Person("Alex", 500, 40);
+        Cat cat = new Cat("Barsik", 700, 30);
+        Robot robot = new Robot("000001110", 200, 0);
+        Wall wall = new Wall("Wall", 30);
+        Treadmill treadmill = new Treadmill("Treadmill", 450);
+
+        Actions[] actions = {person, cat, robot};
+        Barrier[] barrier = {treadmill, wall};
+
+        for (Actions action :
+                actions) {
+            for (int i = 0; i < barrier.length; i++) {
+                if (!barrier[i].move(action)) {
+                    break;
+                }
+
+            }
+            }
+        }
     }
-    Person person = new Person("Alex",500,40);
-    Cat cat = new Cat("Barsik",700,30);
-    Robot robot = new Robot("000001110",200,0);
-    Wall wall = new Wall("Wall",30);
-    Treadmill treadmill = new Treadmill("Treadmill", 450);
-
-    Actions[] actions = {person, cat, robot};
-    Barrier[] barrier = {treadmill,wall};
 
 
-}
